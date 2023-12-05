ef760f6be88ceb339b547a58518640b4e6899d14
MOUAD <mouad159mouad@gmail.com>
2023-12-05 19:15:55 +0100

    Static version

---
+++ lib/widgets/products/product_widget.dart
@@ -0,0 +1,101 @@
+import 'package:e_com_app/screens/inner_screens/product_details.dart';
+import 'package:e_com_app/widgets/subtitle_text.dart';
+import 'package:e_com_app/widgets/title_text.dart';
+import 'package:fancy_shimmer_image/fancy_shimmer_image.dart';
+import 'package:flutter/material.dart';
+
+import '../../consts/app_constants.dart';
+import 'heart_btn.dart';
+
+class ProductWidget extends StatefulWidget {
+  const ProductWidget({super.key});
+
+  @override
+  State<ProductWidget> createState() => _ProductWidgetState();
+}
+
+class _ProductWidgetState extends State<ProductWidget> {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Padding(
+      padding: const EdgeInsets.all(3.0),
+      child: GestureDetector(
+        onTap: () async {
+          await Navigator.pushNamed(context, ProductDetails.routName);
+        },
+        child: Column(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(30.0),
+              child: FancyShimmerImage(
+                imageUrl: AppConstants.productImageUrl,
+                width: double.infinity,
+                height: size.height * 0.22,
+              ),
+            ),
+            const SizedBox(
+              height: 15.0,
+            ),
+            Row(
+              children: [
+                Flexible(
+                  flex: 5,
+                  child: TitlesTextWidget(
+                    label: "Title " * 10,
+                    maxLines: 2,
+                    fontSize: 18,
+                  ),
+                ),
+                const Flexible(
+                  flex: 2,
+                  child: HeartButtonWidget(),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 15.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 5),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Flexible(
+                    flex: 3,
+                    child: SubtitleTextWidget(label: "166.5\$"),
+                  ),
+                  Flexible(
+                    child: Material(
+                      borderRadius: BorderRadius.circular(16.0),
+                      color: Colors.lightBlue,
+                      child: InkWell(
+                        splashColor: Colors.red,
+                        borderRadius: BorderRadius.circular(16.0),
+                        onTap: () {},
+                        child: const Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Icon(
+                            Icons.add_shopping_cart_rounded,
+                            size: 20,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  // SizedBox(
+                  //   width: 1,
+                  // ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
